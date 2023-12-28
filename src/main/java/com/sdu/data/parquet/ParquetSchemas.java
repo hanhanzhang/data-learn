de115401f054d1cd065bae586b3b0d869e75906b
hanhan.zhang <hanzhang800418@163.com>
2023-12-28 09:07:46 +0800

    schema

--- src/main/java/com/sdu/data/parquet/ParquetSchemas.java
+++ src/main/java/com/sdu/data/parquet/ParquetSchemas.java
@@ -2,13 +2,28 @@ package com.sdu.data.parquet;
 
 import com.sdu.data.type.RowType;
 import org.apache.parquet.schema.MessageType;
+import org.apache.parquet.schema.Type;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParquetSchemas {
+
+    public static final String SDU_LAKE = "sdu-lake";
 
     private ParquetSchemas() { }
 
     public static MessageType convertParquetSchema(RowType rowType) {
-        throw new RuntimeException();
+        List<Type> types = new ArrayList<>();
+        for (int i = 0; i < rowType.getFieldCount(); ++i) {
+            com.sdu.data.type.Type type = rowType.getFieldType(i);
+            if (type.isPrimary()) {
+
+            } else {
+
+            }
+        }
+        return new MessageType(SDU_LAKE, types);
     }
 
 }
