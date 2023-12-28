cb5987780fa1f9d2a68b9bc69cf0ca822692bd0f
hanhan.zhang <hanhan.zhang@zhanghan16.local>
2023-12-28 15:24:09 +0800

    parquet

--- src/main/java/com/sdu/data/parquet/ParquetSchemas.java
+++ src/main/java/com/sdu/data/parquet/ParquetSchemas.java
@@ -1,15 +1,27 @@
 package com.sdu.data.parquet;
 
-import com.sdu.data.type.RowType;
-import org.apache.parquet.schema.MessageType;
-import org.apache.parquet.schema.Type;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.parquet.schema.GroupType;
+import org.apache.parquet.schema.MessageType;
+import org.apache.parquet.schema.PrimitiveType;
+import org.apache.parquet.schema.Type;
+import org.apache.parquet.schema.Types;
+
+import com.sdu.data.type.BasicType;
+import com.sdu.data.type.RowType;
+
 public class ParquetSchemas {
 
-    public static final String SDU_LAKE = "sdu-lake";
+    public static final String ROW_MESSAGE = "message";
+
+    public static final String ARRAY_NAME = "list";
+    public static final String ARRAY_ELEMENT_NAME = "element";
+
+    public static final String MAP_NAME = "key_value";
+    public static final String MAP_KEY_NAME = "key";
+    public static final String MAP_VALUE_NAME = "value";
 
     private ParquetSchemas() { }
 
@@ -17,13 +29,69 @@ public class ParquetSchemas {
         List<Type> types = new ArrayList<>();
         for (int i = 0; i < rowType.getFieldCount(); ++i) {
             com.sdu.data.type.Type type = rowType.getFieldType(i);
-            if (type.isPrimary()) {
-
-            } else {
-
-            }
+            String fieldName = rowType.getFieldName(i);
+            types.add(createParquetType(fieldName, type));
         }
-        return new MessageType(SDU_LAKE, types);
+        return new MessageType(ROW_MESSAGE, types);
     }
 
+    private static Type createParquetType(String name, com.sdu.data.type.Type type) {
+        if (type.isPrimary()) {
+            return createParquetPrimaryType(name, (BasicType) type);
+        }
+        return createParquetGroupType(name, type);
+    }
+
+    private static PrimitiveType createParquetPrimaryType(String name, BasicType type) {
+        switch (type.type()) {
+            case INT:
+                return Types.primitive(PrimitiveType.PrimitiveTypeName.INT32, Type.Repetition.REQUIRED)
+                            .named(name);
+            case FLOAT:
+                return Types.primitive(PrimitiveType.PrimitiveTypeName.FLOAT, Type.Repetition.REQUIRED)
+                        .named(name);
+            case DOUBLE:
+                return Types.primitive(PrimitiveType.PrimitiveTypeName.DOUBLE, Type.Repetition.REQUIRED)
+                        .named(name);
+            case STRING:
+                return Types.primitive(PrimitiveType.PrimitiveTypeName.BINARY, Type.Repetition.REQUIRED)
+                        .named(name);
+            case BOOLEAN:
+                return Types.primitive(PrimitiveType.PrimitiveTypeName.BOOLEAN, Type.Repetition.REQUIRED)
+                        .named(name);
+            default:
+                throw new UnsupportedOperationException("unsupported basic type: " + type.type());
+        }
+    }
+
+    private static GroupType createParquetGroupType(String name, com.sdu.data.type.Type type) {
+        switch (type.type()) {
+            // <list-repetition> group <name> {
+            //   repeated group list {
+            //     <element-repetition> <element-type> element;
+            //   }
+            // }
+            case LIST:
+                com.sdu.data.type.ListType listType = (com.sdu.data.type.ListType) type;
+                return Types.list(Type.Repetition.OPTIONAL)
+                        .element(createParquetType(ARRAY_ELEMENT_NAME, listType.getElementType()))
+                        .named(name);
+
+            // <map-repetition> group <name> {
+            //    repeated group key_value {
+            //       <key-repetition> <key-type> key_name;
+            //       <value-repetition> <key-type> value_name;
+            //    }
+            // }
+            case MAP:
+                com.sdu.data.type.MapType mapType = (com.sdu.data.type.MapType) type;
+                return Types.map(Type.Repetition.OPTIONAL)
+                        .key(createParquetType(MAP_KEY_NAME, mapType.getKeyType()))
+                        .value(createParquetType(MAP_VALUE_NAME, mapType.getValueType()))
+                        .named(name);
+
+            default:
+                throw new UnsupportedOperationException("unsupported complex type: " + type.type());
+        }
+    }
 }
--- src/main/java/com/sdu/data/parquet/writer/RowDataParquetWriter.java
+++ src/main/java/com/sdu/data/parquet/writer/RowDataParquetWriter.java
@@ -1,12 +1,22 @@
 package com.sdu.data.parquet.writer;
 
-import com.sdu.data.parquet.RowData;
-import com.sdu.data.type.RowType;
-import com.sdu.data.type.Type;
+import static com.sdu.data.parquet.ParquetSchemas.ARRAY_ELEMENT_NAME;
+import static com.sdu.data.parquet.ParquetSchemas.ARRAY_NAME;
+import static com.sdu.data.parquet.ParquetSchemas.MAP_KEY_NAME;
+import static com.sdu.data.parquet.ParquetSchemas.MAP_NAME;
+import static com.sdu.data.parquet.ParquetSchemas.MAP_VALUE_NAME;
+
+import java.io.Serializable;
+import java.util.Map;
+
 import org.apache.parquet.io.api.Binary;
 import org.apache.parquet.io.api.RecordConsumer;
 
-import java.io.Serializable;
+import com.sdu.data.parquet.RowData;
+import com.sdu.data.type.ListType;
+import com.sdu.data.type.MapType;
+import com.sdu.data.type.RowType;
+import com.sdu.data.type.Type;
 
 public class RowDataParquetWriter {
 
@@ -15,7 +25,7 @@ public class RowDataParquetWriter {
 
     public RowDataParquetWriter(RecordConsumer recordConsumer, RowType rowType) {
         this.recordConsumer = recordConsumer;
-        this.fieldWriters = buildRowFieldWriter(rowType, recordConsumer);
+        this.fieldWriters = buildRowFieldWriter(rowType);
     }
 
     public void writeRow(RowData rowData) {
@@ -23,9 +33,8 @@ public class RowDataParquetWriter {
         for (int i = 0; i < rowData.getFieldCount(); ++i) {
             Object fieldValue = rowData.getFieldValue(i);
             String fieldName = rowData.getFieldName(i);
-            //
             recordConsumer.startField(fieldName, i);
-            fieldWriters[i].write(fieldValue);
+            fieldWriters[i].write(recordConsumer, fieldValue);
             recordConsumer.endField(fieldName, i);
         }
         recordConsumer.endMessage();
@@ -33,33 +42,83 @@ public class RowDataParquetWriter {
 
     public interface FieldWriter extends Serializable {
 
-        void write(Object value);
+        void write(RecordConsumer consumer, Object value);
 
     }
 
-    private static FieldWriter[] buildRowFieldWriter(RowType rowType, RecordConsumer consumer) {
+    private static FieldWriter[] buildRowFieldWriter(RowType rowType) {
         FieldWriter[] fieldWriters = new FieldWriter[rowType.getFieldCount()];
         for (int i = 0; i < rowType.getFieldCount(); ++i) {
-            fieldWriters[i] = buildFieldWriter(rowType.getFieldType(i), consumer);
+            fieldWriters[i] = buildFieldWriter(rowType.getFieldType(i));
         }
         return fieldWriters;
     }
 
-    private static FieldWriter buildFieldWriter(Type type, RecordConsumer consumer) {
+    private static FieldWriter buildFieldWriter(final Type type) {
         switch (type.type()) {
             case INT:
-                return value -> consumer.addInteger((int) value);
+                return (consumer, value) -> consumer.addInteger((Integer) value);
             case FLOAT:
-                return value -> consumer.addFloat((float) value);
+                return (consumer, value) -> consumer.addFloat((float) value);
             case DOUBLE:
-                return value -> consumer.addDouble((double) value);
+                return (consumer, value) -> consumer.addDouble((double) value);
             case STRING:
-                return value -> consumer.addBinary(Binary.fromString((String) value));
+                return (consumer, value) -> consumer.addBinary(Binary.fromString((String) value));
             case BOOLEAN:
-                return value -> consumer.addBoolean((boolean) value);
+                return (consumer, value) -> consumer.addBoolean((boolean) value);
             case LIST:
-
+                // <list-repetition> group <name> {
+                //   repeated group list {
+                //     <element-repetition> <element-type> element;
+                //   }
+                // }
+                ListType listType = (ListType) type;
+                FieldWriter elementWriter = buildFieldWriter(listType.getElementType());
+                return (consumer, value) -> {
+                    consumer.startGroup();
+                    consumer.startField(ARRAY_NAME, 0);
+                    Object[] elements = (Object[]) value;
+                    for (int i = 0; i < elements.length; ++i) {
+                        consumer.startGroup();
+                        consumer.startField(ARRAY_ELEMENT_NAME, 0);
+                        elementWriter.write(consumer, elements[i]);
+                        consumer.endField(ARRAY_ELEMENT_NAME, 0);
+                        consumer.endGroup();
+                    }
+                    consumer.endField(ARRAY_NAME, 0);
+                    consumer.endGroup();
+                };
             case MAP:
+                // <map-repetition> group <name> {
+                //    repeated group key_value {
+                //       <key-repetition> <key-type> key_name;
+                //       <value-repetition> <key-type> value_name;
+                //    }
+                // }
+                MapType mapType = (MapType) type;
+                FieldWriter keyWriter = buildFieldWriter(mapType.getKeyType());
+                FieldWriter valueWriter = buildFieldWriter(mapType.getValueType());
+                return (consumer, value) -> {
+                    @SuppressWarnings("unchecked")
+                    Map<Object, Object> valueMap = (Map<Object, Object>) value;
+                    consumer.startGroup();
+                    consumer.startField(MAP_NAME, 0);
+                    for (Map.Entry<Object, Object> entry : valueMap.entrySet()) {
+                        consumer.startGroup();
+
+                        consumer.startField(MAP_KEY_NAME, 0);
+                        keyWriter.write(consumer, entry.getKey());
+                        consumer.endField(MAP_KEY_NAME, 0);
+
+                        consumer.startField(MAP_VALUE_NAME, 1);
+                        valueWriter.write(consumer, entry.getValue());
+                        consumer.endField(MAP_VALUE_NAME, 1);
+
+                        consumer.endGroup();
+                    }
+                    consumer.endField(MAP_NAME, 0);
+                    consumer.endGroup();
+                };
 
             default:
                 throw new UnsupportedOperationException("unsupported type: " + type.type());
