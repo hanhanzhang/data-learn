bdec6dbc6d8595e5629894b7f9921594f36959dd
hanhan.zhang <hanzhang800418@163.com>
2024-05-06 23:22:51 +0800

    update

--- flink-learn/src/main/java/com/sdu/data/flink/FlinkDynamicVariablesBootstrap.java
+++ flink-learn/src/main/java/com/sdu/data/flink/FlinkDynamicVariablesBootstrap.java
@@ -1,35 +1,29 @@
 package com.sdu.data.flink;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
+import com.sdu.data.common.JsonUtils;
 import org.apache.flink.api.common.DynamicConfig;
 import org.apache.flink.api.common.functions.RichFilterFunction;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.shaded.guava31.com.google.common.collect.Sets;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.sdu.data.common.JsonUtils;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
-/**
- * submit:
- *  ./bin/flink run -d -c com.sdu.data.flink.FlinkDynamicPropertiesBootstrap /Users/hanhan.zhang/project/data-learn/flink-learn/target/flink-learn-1.0-SNAPSHOT.jar
- * update property:
- *
- * */
-public class FlinkDynamicPropertiesBootstrap implements Bootstrap {
+/** Dynamic variables bootstrap. */
+public class FlinkDynamicVariablesBootstrap implements Bootstrap {
 
-    public static class WordFilterWithDynamicParameterFunction extends RichFilterFunction<String> {
+    public static class WordFilterWithDynamicVariablesFunction extends RichFilterFunction<String> {
 
         private static final Logger LOG =
-                LoggerFactory.getLogger(WordFilterWithDynamicParameterFunction.class);
+                LoggerFactory.getLogger(WordFilterWithDynamicVariablesFunction.class);
 
-        private static final String KEY = "dynamic.props.filter.words";
+        private static final String KEY = "filters";
 
         private transient long timestamp = 0;
         private transient int subtask;
@@ -43,29 +37,34 @@ public class FlinkDynamicPropertiesBootstrap implements Bootstrap {
             this.config = getRuntimeContext().getDynamicConfig();
             this.filterWords = new HashMap<>();
             this.timestamp = System.currentTimeMillis();
-            LOG.info("Task started, default dynamic property : {} = {}", KEY, config.getDynamicVariable(KEY, "[]"));
+            LOG.info(
+                    "Task started, default dynamic variable : {} = {}",
+                    KEY,
+                    config.getDynamicVariable(KEY, ""));
         }
 
         @Override
         public boolean filter(String word) throws Exception {
             if (subtask == 0 && (System.currentTimeMillis() - timestamp) >= 2 * 60 * 1000L) {
-                throw new RuntimeException("should restart.");
+                throw new RuntimeException("restart again.");
             }
-            String value = config.getDynamicVariable(KEY, "[]");
-            Set<String> filters = getAndRemove(filterWords, value, config.getTotalDynamicVariables());
+            String value = config.getDynamicVariable(KEY, "");
+            Set<String> filters =
+                    getAndRemove(filterWords, value, config.getTotalDynamicVariables());
             return !filters.contains(word);
         }
 
-        private static Set<String> getAndRemove(Map<String, Set<String>> filterWords,
-                String value, Map<String, String> variables) throws Exception {
+        private static Set<String> getAndRemove(
+                Map<String, Set<String>> filterWords, String value, Map<String, String> variables)
+                throws Exception {
             Set<String> filters = filterWords.get(value);
             if (filters != null) {
                 return filters;
             }
             filterWords.clear();
-            LOG.info("received dynamic property, key: {}, value: {}", KEY, value);
+            LOG.info("receive dynamic variable, key: {}, value: {}", KEY, value);
             LOG.info("total dynamic variables: {}", JsonUtils.toJson(variables));
-            filters = JsonUtils.fromJson(value, new TypeReference<Set<String>>() {});
+            filters = Sets.newHashSet(value.split(","));
             filterWords.put(value, filters);
             return filters;
         }
@@ -77,15 +76,14 @@ public class FlinkDynamicPropertiesBootstrap implements Bootstrap {
         DataStream<String> sourceStream = Bootstrap.wordSourceStream(env, 2);
 
         //
-        sourceStream.filter(new WordFilterWithDynamicParameterFunction())
-                .name("filter_with_dynamic_property")
+        sourceStream
+                .filter(new WordFilterWithDynamicVariablesFunction())
+                .name("filter_with_dynamic_variables")
                 .setParallelism(2)
                 .addSink(new PrintSinkFunction<>())
-                .name("sink")
+                .name("sink_console")
                 .setParallelism(2);
 
         env.execute();
     }
-
-
 }
